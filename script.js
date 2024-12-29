36a8207bd74878f06f9dde804214050ffa32bf8e
vasu07017 <118751603+vasu07017@users.noreply.github.com>
2024-12-29 15:55:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-  
-    alert(`Thank you, ${name}! We have received your message.`);
-  });
-  
